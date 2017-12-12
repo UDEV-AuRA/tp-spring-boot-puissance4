fa1a49e0dcdc36b8a298b10745e9217abdfb9351
Alexandre <alexandre.petrillo@gmail.com>
2017-12-12 20:18:48 +0100

    ajout controller et page

--- src/main/java/com/igs/ipi/tpspringbootpetrillo/controller/GameController.java
+++ src/main/java/com/igs/ipi/tpspringbootpetrillo/controller/GameController.java
@@ -1,4 +1,15 @@
-package com.igs.ipi.tpspringbootpetrillo;
+package com.igs.ipi.tpspringbootpetrillo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class GameController {
+
+    @RequestMapping("/game/new")
+    public ModelAndView newGame() {
+        ModelAndView mv = new ModelAndView("game");
+        return mv;
+    }
 }
