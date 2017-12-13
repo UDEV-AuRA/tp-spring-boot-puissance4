2895f8853a54a9c9bfb26f5ed4e7f5e44f903d0e
Alexandre <alexandre.petrillo@gmail.com>
2017-12-13 14:31:56 +0100

    partie en session

--- src/main/java/com/igs/ipi/tpspringbootpetrillo/controller/GameController.java
+++ src/main/java/com/igs/ipi/tpspringbootpetrillo/controller/GameController.java
@@ -2,6 +2,7 @@ package com.igs.ipi.tpspringbootpetrillo.controller;
 
 import com.igs.ipi.tpspringbootpetrillo.model.GameModel;
 import com.igs.ipi.tpspringbootpetrillo.service.GameService;
+import com.igs.ipi.tpspringbootpetrillo.service.PartieEnCours;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,15 +11,26 @@ import org.springframework.web.servlet.ModelAndView;
 public class GameController {
 
     private GameService gameService;
+    private PartieEnCours partieEnCours;
 
-    public GameController(GameService gameService) {
+    public GameController(GameService gameService, PartieEnCours partieEnCours) {
         this.gameService = gameService;
+        this.partieEnCours = partieEnCours;
     }
 
     @RequestMapping("/game/new")
     public ModelAndView newGame() {
         ModelAndView mv = new ModelAndView("game");
         GameModel gameModel = gameService.newGame();
+        partieEnCours.setGameModel(gameModel);
+        mv.addObject("game", gameModel);
+        return mv;
+    }
+
+    @RequestMapping("/game")
+    public ModelAndView game() {
+        ModelAndView mv = new ModelAndView("game");
+        GameModel gameModel = partieEnCours.getGameModel();
         mv.addObject("game", gameModel);
         return mv;
     }
