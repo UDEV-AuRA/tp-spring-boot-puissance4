ee9293b9a6ba932ca7e99e75fc36c4c8a1ae9d0b
Alexandre <alexandre.petrillo@gmail.com>
2017-12-14 11:38:48 +0100

    css et images

--- src/main/java/com/igs/ipi/tpspringbootpetrillo/model/Jeton.java
+++ src/main/java/com/igs/ipi/tpspringbootpetrillo/model/Jeton.java
@@ -1,5 +1,17 @@
 package com.igs.ipi.tpspringbootpetrillo.model;
 
 public enum Jeton {
-    ROUGE, JAUNE, VIDE
+    ROUGE, JAUNE, VIDE;
+
+    public String getCode() {
+
+        switch (this) {
+            case ROUGE:
+                return "rouge.png";
+            case JAUNE:
+                return "bleu.png";
+            default:
+                return null;
+        }
+    }
 }
