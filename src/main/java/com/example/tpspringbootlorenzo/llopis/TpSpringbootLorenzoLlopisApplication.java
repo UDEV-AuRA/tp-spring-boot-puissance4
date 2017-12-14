3043c4146ff6b9c63d905e413182fe680547bf90
zllopis <lorenzo.llopis@gmail.com>
2017-12-14 13:47:14 +0100

    1er sauv

---
+++ src/main/java/com/example/tpspringbootlorenzo/llopis/TpSpringbootLorenzoLlopisApplication.java
@@ -0,0 +1,12 @@
+package com.example.tpspringbootlorenzo.llopis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSpringbootLorenzoLlopisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpSpringbootLorenzoLlopisApplication.class, args);
+	}
+}
