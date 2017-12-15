ec3bdd94bbeb8f0af188deb74fd3b807c8ff1edd
Julien <juju.om26@gmail.com>
2017-12-15 10:55:20 +0100

    partie en session

--- tp-springboot-_julienpinteaux_/src/main/java/com/igs/ipi/tpspringboot/julienpinteaux/Controller/GameController.java
+++ tp-springboot-_julienpinteaux_/src/main/java/com/igs/ipi/tpspringboot/julienpinteaux/Controller/GameController.java
@@ -2,6 +2,7 @@ package com.igs.ipi.tpspringboot.julienpinteaux.Controller;
 
 import com.igs.ipi.tpspringboot.julienpinteaux.Model.GameModel;
 import com.igs.ipi.tpspringboot.julienpinteaux.Service.GameService;
+import com.igs.ipi.tpspringboot.julienpinteaux.Session.PartieEnCours;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +16,29 @@ public class GameController {
     @Autowired
     private GameService gameService;
 
+    @Autowired
+    private PartieEnCours partieEnCours;
+
     @GetMapping("/new")
     public ModelAndView newGame() {
         GameModel game = gameService.newGame();
+        ModelAndView mav = new ModelAndView("game");
+        mav.addObject("game",game);
+        partieEnCours.setGame(game);
+        return mav;
+    }
+
+    @GetMapping
+    public ModelAndView continueGame(){
+        GameModel game = partieEnCours.getGame();
+        ModelAndView mav = new ModelAndView("game");
+        mav.addObject("game",game);
+        return mav;
+    }
+
+    @GetMapping ("/drop/{i}")
+    public ModelAndView dropIn(){
+        GameModel game = partieEnCours.getGame();
         ModelAndView mav = new ModelAndView("game");
         mav.addObject("game",game);
         return mav;
