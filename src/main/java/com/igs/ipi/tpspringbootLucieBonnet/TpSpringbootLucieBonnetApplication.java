42eb2b766f0888c2f29d3edb9f1ed9dc67fe90f0
LucieBnnt <33019137+LucieBnnt@users.noreply.github.com>
2018-01-07 13:40:19 +0100

    Add files via upload

---
+++ src/main/java/com/igs/ipi/tpspringbootLucieBonnet/TpSpringbootLucieBonnetApplication.java
@@ -0,0 +1,12 @@
+package com.igs.ipi.tpspringbootLucieBonnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSpringbootLucieBonnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpSpringbootLucieBonnetApplication.class, args);
+	}
+}
